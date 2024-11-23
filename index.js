8e60b5f7c9665983a770e7f90188015cd694afe9
prasasdi <rizky.adie7@gmail.com>
2024-11-23 23:23:53 +0700

    And here's Meg.

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 $(document).ready(function() {
     $('.thiss').click(function() {
         $("#c-body p:first-of-type").css("margin-bottom", "0px");
-        if ($("#c-body p:nth-of-type(2)").html() == undefined)
+        if ($("#c-body p:nth-of-type(2)").html() == undefined || !$(this).hasClass('dude'))
         {
             $.get("routes/about.html", function(data){
-                $("#c-body").append(data);
+                if ($("#c-body p:nth-of-type(2)").html() != undefined) {
+                    $("#c-body p:nth-of-type(2)").html(data);
+                } else {
+                    $("#c-body").append(data);
+                }
             });
+            $(this).addClass('dude');
         }
         else{
             $("#c-body p:nth-of-type(2)").html("<p class=\"text-muted\">I mean, why can't i just go HTML and JS and deploy them into static pages? Anyway, i do mainly pretty much in backend using .NET</p>");
+            $(this).removeClass('dude');
         }
     });
 });
