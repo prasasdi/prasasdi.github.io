c0d9f6ea883d303a82a1d7e6dcaac92fc41be5bd
prasasdi <rizky.adie7@gmail.com>
2024-11-23 22:54:35 +0700

    just go

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 $(document).ready(function() {
-    // Menggunakan jQuery untuk mengatur event click pada elemen dengan id "about"
     $('#btn-about').click(function() {
-        // Mengubah isi dari elemen dengan id "about"
         $(this).html('<h2>Tentang</h2><p>Isi ini telah diubah ketika diklik!</p>');
+    });
+
+    $('.thiss').click(function() {
+        $("#c-body p:first-of-type").css("margin-bottom", "0px");
+        $.get("routes/about.html", function(data){
+            $("#c-body").append(data);
+        });
+        console.log("iki loh cah");
     });
 });
