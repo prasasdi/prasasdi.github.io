518ddcc3d1602d8aaa12655a93e2eb5956db11a6
prasasdi <rizky.adie7@gmail.com>
2024-11-23 23:01:18 +0700

    just goes

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 $(document).ready(function() {
-    $('#btn-about').click(function() {
-        $(this).html('<h2>Tentang</h2><p>Isi ini telah diubah ketika diklik!</p>');
-    });
-
     $('.thiss').click(function() {
         $("#c-body p:first-of-type").css("margin-bottom", "0px");
-        $.get("routes/about.html", function(data){
-            $("#c-body").append(data);
-        });
-        console.log("iki loh cah");
+        if ($("#c-body p:nth-of-type(2)").html() == undefined)
+        {
+            $.get("routes/about.html", function(data){
+                $("#c-body").append(data);
+            });
+        }
     });
 });
