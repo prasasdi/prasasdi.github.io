beb4be0e697ed63bc991751d81b589e88d46cc00
prasasdi <rizky.adie7@gmail.com>
2024-11-23 23:13:53 +0700

    and iiiii will always love uuuuu <3

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 $(document).ready(function() {
     $('.thiss').click(function() {
         $("#c-body p:first-of-type").css("margin-bottom", "0px");
-        if ($("#c-body p:nth-of-type(2)").html() == undefined)
+        if ($("#c-body p:nth-of-type(2)").html() == undefined && $(this).hasClass('dude'))
         {
             $.get("routes/about.html", function(data){
-                $("#c-body").append(data);
+                $("#c-body p:nth-of-type(2)").html(data);
             });
+            $(this).removeClass('dude');
+
+        }
+        else{
+            $("#c-body p:nth-of-type(2)").html("<p class=\"text-muted\">I mean, why can't i just go HTML and JS and deploy them into static pages? Anyway, i do mainly pretty much in backend using .NET</p>");
+            $(this).addClass("dude");
         }
     });
 });
